72258c6f43b1471f7aa081495b2dbf2e7bcce0d2
Moose919 <marcus.wreford91@gmail.com>
2024-10-16 17:20:49 +0100

    draft

---
+++ Week01/split_bill.py
@@ -0,0 +1,14 @@
+#varables for bill 
+total_bill = 84.87
+number_of_friends = 3
+amount_per_friend = total_bill / number_of_friends
+print("how much each friend owes",amount_per_freind)
+ # Round up the amount per friend to the nearest pound
+rounded = round(amount_per_friend)
+# Calculate the extra tip needed
+extra_tip = rounded - amount_per_friend
+print(" exstra tip =", extra_tip)
+# Calculate the total amount paid and total tip paid
+  total_paid = roundedt*number_of_friends
+  total_tip = total_paid - total_bill
+print(" the total bill is =",total_bill)
